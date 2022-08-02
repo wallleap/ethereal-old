00c4ad0a3462264f742bcab44cd114aa79dc7f36
wallleap <15579576761@163.com>
2022-08-02 20:28:59 +0800

    💄Style(View)：更改导航栏图标及loading动画

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -10,7 +10,7 @@
         <ul class="nav-wrap">
           <li class="nav-item" v-for="menu in displayMenu" :key="menu.path">
             <RouterLink :to="menu.path">
-              <i :class="['iconfont', `icon-${menu.icon}`]"></i>
+              <i :class="['ethefont', `icon-${menu.icon}`]"></i>
               {{ menu.title }}
             </RouterLink>
           </li>
@@ -19,7 +19,7 @@
 
       <div class="site-admin">
         <a class="add-box" href="//github.com/wallleap/auroraBlog/issues/new" target="_blank">
-          <i class="iconfont icon-bianji"></i>
+          <i class="ethefont icon-bianji"></i>
         </a>
       </div>
     </div>
@@ -35,14 +35,14 @@ export default {
     const { archiveOpts, categoryOpts, tagOpts, inspirationOpts, bookOpts, friendOpts, aboutOpts } = this.$config
     return {
       menu: [
-        { path: '/', display: true, icon: 'homefill', title: '首页' },
-        { path: '/archive', display: archiveOpts.display, icon: 'archive', title: '归档' },
-        { path: '/category', display: categoryOpts.display, icon: 'fenlei1', title: '分类' },
-        { path: '/tag', display: tagOpts.display, icon: 'tag', title: '标签' },
-        { path: '/book', display: bookOpts.display, icon: 'book', title: '书单' },
-        { path: '/inspiration', display: inspirationOpts.display, icon: 'linggan', title: '灵感' },
-        { path: '/friend', display: friendOpts.display, icon: 'friendaddfill', title: '友链' },
-        { path: '/about', display: aboutOpts.display, icon: 'leaf', title: '关于' },
+        { path: '/', display: true, icon: 'home', title: '首页' },
+        { path: '/archive', display: archiveOpts.display, icon: 'archives', title: '归档' },
+        { path: '/category', display: categoryOpts.display, icon: 'category', title: '分类' },
+        { path: '/tag', display: tagOpts.display, icon: 'tags', title: '标签' },
+        { path: '/book', display: bookOpts.display, icon: 'books', title: '书单' },
+        { path: '/inspiration', display: inspirationOpts.display, icon: 'inspire', title: '灵感' },
+        { path: '/friend', display: friendOpts.display, icon: 'friends', title: '友链' },
+        { path: '/about', display: aboutOpts.display, icon: 'about', title: '关于' },
       ],
     }
   },
--- src/components/Loading/index.vue
+++ src/components/Loading/index.vue
@@ -1,16 +1,45 @@
 <template>
-  <img class="loading" :src="$config.loadingImg" alt />
+  <svg class="loading" viewBox="0 0 50 50">
+    <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="8"></circle>
+  </svg>
 </template>
 
 <style lang="scss" scoped>
 .loading {
-  @include pc-layout {
-    margin: 100px auto;
-    width: 100px;
+  animation: rotate 2s linear infinite;
+  z-index: 2;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -25px 0 0 -25px;
+  width: 50px;
+  height: 50px;
+
+  & .path {
+    stroke: $primary;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
   }
-  @include sp-layout {
-    margin: 150px auto;
-    width: 80px;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 150;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -35;
+  }
+  100% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -124;
   }
 }
 </style>
